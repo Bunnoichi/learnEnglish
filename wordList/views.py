68c8c7f1c5e88b54ea463176f1645547ab203bae
Bunnoichi <furuto.3866.miyago@gmail.com>
2025-01-12 06:17:08 +0900

    単語をランダムに出現させる仕様に変更 出題回数がnullの場合の例外処理を設定

--- wordList/views.py
+++ wordList/views.py
@@ -55,20 +55,26 @@ class CardUpdateView(View):
 
 class LessonSessionView(View):
    def get(self, request):
-      card = Card.objects.order_by('created_at')[0]
+      card = Card.objects.order_by('?')[0]
       print(card)
       return render(request, "wordList/lesson_session.html", {"card": card})
    
    def post(self, request):
       data = Card.objects.get(pk=request.POST['id'])
-      data.count_appear += 1
+
+      try:
+         data.count_appear += 1
+      except:
+         data.count_appear = 0
+         data.count_appear += 1
+         data.count_incorrect = 0
 
       if "incorrect" in request.POST:
          data.count_incorrect += 1
       
       data.save()
 
-      card = Card.objects.order_by('created_at')[0]
+      card = Card.objects.order_by('?')[0]
       return render(request, "wordList/lesson_session.html", {"card": card})
 
 index = IndexView.as_view()
